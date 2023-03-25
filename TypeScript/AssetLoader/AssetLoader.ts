31a093beade3794e3f4c7e62f06b480e24bf8ec6
Gradess <trofimovjava@gmail.com>
2023-03-25 21:08:48 +0100

    Add asset loading

--- TypeScript/AssetLoader/AssetLoader.ts
+++ TypeScript/AssetLoader/AssetLoader.ts
@@ -1,28 +1,64 @@
-class AssetLoader {
-    assets : Asset[];
-    assets_loaded : number;
-    assets_to_load : number;
+import { Asset } from "./Asset.js";
+
+export class AssetLoader {
+    public on_asset_list_obtained_event: Function[];
+    public on_all_assets_loaded_event: Function[];
+
+    private assets : Asset[];
+    private assets_loaded : number;
+    private assets_to_load : number;
 
     constructor() {
         this.assets = [];
         this.assets_loaded = 0;
         this.assets_to_load = 0;
+        this.on_asset_list_obtained_event = [];
+        this.on_all_assets_loaded_event = [];
     }
 
-    add_asset(asset : Asset) {
+    public load() {
+        this.on_asset_list_obtained_event.push(this.create_assets_from_list.bind(this));
+        this.on_asset_list_obtained_event.push(this.load_assets.bind(this));
+        this.on_all_assets_loaded_event.push(this.load_assets.bind(this));
+        this.download_asset_list();
+    }
+
+    private add_asset(asset : Asset) {
         this.assets.push(asset);
         this.assets_to_load ++;
     }
 
-    load_assets() {
+    private download_asset_list() {
+        let request = new XMLHttpRequest();
+        request.onload = () => {
+            if (request.status >= 200 && request.status < 400) {
+                let data = JSON.parse(request.responseText);
+                this.on_asset_list_obtained(data);
+            } else {
+                console.log("Could not load assets.json");
+            }
+        };
+
+        request.open("GET", "assets.json", true);
+        request.send();
+    }
+
+    private create_assets_from_list(list : string[]) {
+        for (let i = 0; i < list.length; i++) {
+            let asset = new Asset(list[i]);
+            this.add_asset(asset);
+        }
+    }
+
+    private load_assets() {
         for (let i = 0; i < this.assets.length; i++) {
             let asset = this.assets[i];
-            asset.bind(this.on_asset_loaded);
+            asset.bind(this.on_asset_loaded.bind(this));
             asset.load();
         }
     }
 
-    on_asset_loaded(asset : Asset) {
+    private on_asset_loaded(asset : Asset) {
         asset.unbind(this.on_asset_loaded);
 
         this.assets_loaded ++;
@@ -31,7 +67,16 @@ class AssetLoader {
         }
     }
 
-    on_all_assets_loaded() {
-        console.log("all assets loaded");
+    private on_asset_list_obtained(list: string[]) {
+        this.on_asset_list_obtained_event.forEach((callback) => {
+            callback(list);
+        });
     }
+
+    private on_all_assets_loaded() {
+        this.on_all_assets_loaded_event.forEach((callback) => {
+            callback(this.assets);
+        });
+    }
+
 }
