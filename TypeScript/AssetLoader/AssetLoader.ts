f1768501f1cd9e3b12bc900ffca8865fedd03633
Gradess <trofimovjava@gmail.com>
2023-03-25 12:29:04 +0100

    Add Asset and AssetLoader

---
+++ TypeScript/AssetLoader/AssetLoader.ts
@@ -0,0 +1,37 @@
+class AssetLoader {
+    assets : Asset[];
+    assets_loaded : number;
+    assets_to_load : number;
+
+    constructor() {
+        this.assets = [];
+        this.assets_loaded = 0;
+        this.assets_to_load = 0;
+    }
+
+    add_asset(asset : Asset) {
+        this.assets.push(asset);
+        this.assets_to_load ++;
+    }
+
+    load_assets() {
+        for (let i = 0; i < this.assets.length; i++) {
+            let asset = this.assets[i];
+            asset.bind(this.on_asset_loaded);
+            asset.load();
+        }
+    }
+
+    on_asset_loaded(asset : Asset) {
+        asset.unbind(this.on_asset_loaded);
+
+        this.assets_loaded ++;
+        if (this.assets_loaded == this.assets_to_load) {
+            this.on_all_assets_loaded();
+        }
+    }
+
+    on_all_assets_loaded() {
+        console.log("all assets loaded");
+    }
+}
---
+++ js/AssetLoader/Asset.js
@@ -0,0 +1,85 @@
+"use strict";
+class Asset {
+    constructor(path) {
+        this.path = path;
+        this.callbacks = [];
+    }
+    bind(callback) {
+        this.callbacks.push(callback);
+    }
+    unbind(callback) {
+        this.callbacks = this.callbacks.filter((cb) => cb !== callback);
+    }
+    load() {
+        if (this.path.endsWith(".png") || this.path.endsWith(".jpg") || this.path.endsWith(".jpeg")) {
+            this.load_image();
+        }
+        else if (this.path.endsWith(".mp3") || this.path.endsWith(".wav")) {
+            this.load_audio();
+        }
+        else if (this.path.endsWith(".json")) {
+            this.load_json();
+        }
+        else if (this.path.endsWith(".js")) {
+            this.load_script();
+        }
+        else if (this.path.endsWith(".css")) {
+            this.load_style();
+        }
+        else {
+            console.error("unknown asset type");
+        }
+    }
+    load_image() {
+        var img = new Image();
+        img.src = this.path;
+        if (img.complete) {
+            this.on_asset_loaded();
+        }
+        else {
+            img.onload = () => {
+                this.on_asset_loaded();
+            };
+        }
+    }
+    load_audio() {
+        var audio = new Audio();
+        audio.src = this.path;
+        audio.oncanplaythrough = () => {
+            this.on_asset_loaded();
+        };
+    }
+    load_json() {
+        var request = new XMLHttpRequest();
+        request.open("GET", this.path, true);
+        request.onload = () => {
+            if (request.status >= 200 && request.status < 400) {
+                this.on_asset_loaded();
+            }
+            else {
+                console.error("error loading json: " + this.path);
+            }
+        };
+        request.send();
+    }
+    load_script() {
+        var script = document.createElement("script");
+        script.src = this.path;
+        document.head.appendChild(script);
+        this.on_asset_loaded();
+    }
+    load_style() {
+        var link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = this.path;
+        document.head.appendChild(link);
+        this.on_asset_loaded();
+    }
+    on_asset_loaded() {
+        console.log("asset loaded: " + this.path);
+        for (var i = 0; i < this.callbacks.length; i++) {
+            var callback = this.callbacks[i];
+            callback(this);
+        }
+    }
+}
