23cc99801441d34493369f80810db94cf3e78fcf
Gradess <trofimovjava@gmail.com>
2023-03-26 12:18:41 +0200

    Add random fact

--- TypeScript/EntryPage/EntryPage.ts
+++ TypeScript/EntryPage/EntryPage.ts
@@ -1,16 +1,25 @@
 import { EntryPageProgressBar } from "./EntryPageProgressBar.js";
+import { RandomFact } from "./RandomFact.js";
 
 export class EntryPage {
     private image_path: string;
     private progress_bar: EntryPageProgressBar;
-
+    private random_fact: RandomFact;
 
     constructor() {
         this.image_path = "Images/Background/EntryPageBackground.jpeg";
         this.progress_bar = new EntryPageProgressBar();
+        this.random_fact = new RandomFact();
     }
 
     public run(): void {
+        this.progress_bar.on_animation_end_event.push((event: any) => {
+            const animation_event = event as AnimationEvent;
+            if (animation_event.animationName === "zoomIn") {
+                this.random_fact.run();
+            }
+        });
+
         this.progress_bar.run();
     }
 
---
+++ TypeScript/EntryPage/RandomFact.ts
@@ -0,0 +1,87 @@
+export class RandomFact {
+    private static readonly API_URL = "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en";
+    private static readonly MIN_FACT_DELAY = 7;
+    private static readonly WORDS_PER_SECOND = 3;
+    private advice_container : Element;
+    private advice_text : Element;
+    private current_advice: any;
+
+
+    constructor() {
+        this.advice_container = document.getElementsByClassName("g-advice-container")[0];
+        this.advice_text = document.getElementsByClassName("g-advice-text")[0];
+
+        this.advice_text.addEventListener("animationend", this.on_advice_text_animation_end.bind(this));
+    }
+
+    public run() {
+        this.requestAdvice();
+    };
+
+    private requestAdvice() {
+        fetch(RandomFact.API_URL).then(response => response.json()).then(advice => {
+            this.current_advice = advice;
+            let delay = this.calculate_delay(advice);
+
+            if (this.is_hidden()) {
+                this.update_advice();
+                this.show();
+                delay += 2;
+            } else {
+                this.fade_out_advice_text();
+            }
+
+            setTimeout(() => this.requestAdvice(), delay * 1000);
+            
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    private update_advice() {
+        this.advice_text.innerHTML = this.current_advice.text;
+    }
+
+    private is_hidden() : boolean {
+        return this.advice_container.classList.contains("visually-hidden");
+    }
+
+    private show() {
+        this.advice_container.classList.remove("visually-hidden");
+        
+        this.advice_container.classList.add("animate__fadeInDownEdit");
+        this.advice_container.classList.add("animate__animated");
+    }
+
+    private fade_in_advice_text() {
+        this.advice_text.classList.remove("animate__fadeOut");
+        this.advice_text.classList.remove("animate__animated");
+
+        this.advice_text.classList.add("animate__fadeIn");
+        this.advice_text.classList.add("animate__animated");
+    }
+
+    private fade_out_advice_text() {
+        this.advice_text.classList.remove("animate__fadeIn");
+        this.advice_text.classList.remove("animate__animated");
+
+        this.advice_text.classList.add("animate__fadeOut");
+        this.advice_text.classList.add("animate__animated");
+    }
+
+    private calculate_delay(advice : any) {
+        const text = advice.text;
+        const words = text.split(" ");
+        const delay = words.length / RandomFact.WORDS_PER_SECOND;
+
+        return Math.max(delay, RandomFact.MIN_FACT_DELAY);
+    }
+
+    private on_advice_text_animation_end(event : Event) {
+        const animationEvent = event as AnimationEvent;
+        if (animationEvent.animationName === "fadeOut") {
+            this.update_advice();
+            this.fade_in_advice_text();
+        }
+    }
+}
